66a90e1ff772dcea35de7591d0d7f6dc7d91c03f
Viki <v.lukacova308@gmail.com>
2025-09-16 15:57:47 +0200

    initial commit with first challenge solution for string to number conversion

---
+++ 8kyuu/convert_string_to_number.py
@@ -0,0 +1,12 @@
+"""
+We need a function that can transform a string into a number.
+
+Examples:
+"1234" --> 1234
+"605"  --> 605
+"1405" --> 1405
+"-7" --> -7
+"""
+
+def string_to_number(s):
+    return int(s)
