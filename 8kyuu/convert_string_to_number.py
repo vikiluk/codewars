1afbce640ead2f0ec9888f88bec24c3e9248a55e
Viki <v.lukacova308@gmail.com>
2025-09-17 10:47:03 +0200

    added more details to convert_string_to_number.py description

--- 8kyuu/convert_string_to_number.py
+++ 8kyuu/convert_string_to_number.py
@@ -1,7 +1,9 @@
 """
-We need a function that can transform a string into a number.
+NAME: Convert a String to a Number!
+DESCRIPTION: We need a function that can transform a string into a number.
+KATA URL: https://www.codewars.com/kata/544675c6f971f7399a000e79
 
-Examples:
+EXAMPLES:
 "1234" --> 1234
 "605"  --> 605
 "1405" --> 1405
